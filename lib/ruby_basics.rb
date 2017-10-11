2788f9e7a3e407c42fbb491d5ff46ba7bdf4e5e9
xinyanc3694 <xinyanc3694@hstat.org>
2017-10-11 00:11:03 +0000

    fix errors

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,24 @@
-def division
-    
+def division(num_one,num_two)
+    num_one/num_two
 end
 
-def argue
-  
+def argue(sent)
+    "I'm right and you are wrong!"
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
 def return_a_value
+    "Nice"
     
 end
 
-def pizza_party
-    
+def pizza_party(pizza="cheese")
+    pizza
 end
 
-def whisper
-    
+def whisper(word)
+    word.downcase
 end
